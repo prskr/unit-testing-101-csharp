b45320b7a7fb6996c474d423f9538c6e9b1540b7
Andreas Huber <ahuber@serva-ts.com>
2018-05-23 09:50:50 +0200

    Created class library plus tests

---
+++ FizzBuzz.Test/FizzBuzzTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Test
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void Test01()
+        {
+            Assert.AreEqual("1", FizzBuzz.GetResult(1));
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            Assert.AreEqual("fizz", FizzBuzz.GetResult(3));
+        }
+
+        [TestMethod]
+        public void Test05()
+        {
+            Assert.AreEqual("buzz", FizzBuzz.GetResult(5));
+        }
+
+        [TestMethod]
+        public void Test13()
+        {
+            Assert.AreEqual("fizz", FizzBuzz.GetResult(13));
+        }
+
+        [TestMethod]
+        public void Test15()
+        {
+            Assert.AreEqual("fizzbuzz", FizzBuzz.GetResult(15));
+        }
+
+        [TestMethod]
+        public void Test30()
+        {
+            Assert.AreEqual("fizzbuzz", FizzBuzz.GetResult(30));
+        }
+
+    }
+}
---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,10 @@
+﻿namespace FizzBuzz
+{
+    public static class FizzBuzz
+    {
+        public static string GetResult(int number)
+        {
+            return number.ToString();
+        }
+    }
+}
